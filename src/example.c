f1c05ff9d65c9dccbbb4d9b4a3b1df5910599f99
Federico De Felici <fdf8210@gmail.com>
2024-05-18 11:00:18 +0200

    a

---
+++ src/example.c
@@ -0,0 +1,12 @@
+#define CLOVE_SUITE_NAME MySuite01
+#include "clove-unit.h"
+
+CLOVE_TEST(FirstTest) {
+    int a = 1;
+    int b = 1;
+    CLOVE_INT_EQ(a, b);
+}
+
+CLOVE_TEST(SecondTest) {
+    CLOVE_IS_TRUE(1);
+}
